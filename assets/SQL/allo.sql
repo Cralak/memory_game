57ce371e6c77c0826802f96534d294e8f317ffab
ouachtou <pierre.legrand1105@gmail.com>
2023-10-16 15:10:35 +0200

    sql

--- assets/SQL/allo.sql
+++ assets/SQL/allo.sql
@@ -1,53 +1,56 @@
------- TABLE UTILISATEUR -------/
+------------------ TABLE UTILISATEUR ----------------/
 
 
-CREATE TABLE IF  NOT EXIST utilisateur(
+CREATE TABLE IF  NOT EXISTS utilisateur(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    email VARCHAR(191) NOT NULL,
+    email VARCHAR(191) UNIQUE NOT NULL,
     mot_de_passe VARCHAR(200) NOT NULL,
-    pseudo VARCHAR(191) NOT NULL,
+    pseudo VARCHAR(191) UNIQUE NOT NULL,
     date_et_heure DATETIME NOT NULL,
     derniere_connexion DATETIME,
     PRIMARY KEY(id)
 )
 
 CHARACTER SET 'utf8';
+ENGIN=INNOB;
 
 
 
 
------- TABLE SCORE ----------------/
+---------------- TABLE SCORE ----------------/
 
-CREATE TABLE score(
+CREATE TABLE IF  NOT EXISTS score(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     id_joueur INT UNSIGNED NOT NULL,
     id_jeu INT UNSIGNED NOT NULL,
     difficulte ENUM('FACILE,MOYEN,DIFFICIL') NOT NULL,
     score_partie INT UNSIGNED NOT NULL,
     datetime_partie DATETIME NOT NULL,
-    PRIMARY KEY(id)
-
+    PRIMARY KEY(id),
 )
 
 CHARACTER SET 'utf8';
+ENGIN=INNOB;
 
------- TABLE MESSAGE ----------------/
 
-CREATE TABLE message(
+--------------- TABLE MESSAGE ----------------/
+
+CREATE TABLE IF  NOT EXISTS message(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     id_expediteur INT UNSIGNED NOT NULL,
     id_jeu INT UNSIGNED NOT NULL,
     message TEXT NOT NULL,
     datetime_message DATETIME NOT NULL,
-    PRIMARY KEY(id)
-
+    PRIMARY KEY(id),
 )
 
 CHARACTER SET 'utf8';
+ENGIN=INNOB;
 
------- TABLE JEU ----------------/
 
-CREATE TABLE jeu(
+---------------- TABLE JEU ----------------/
+
+CREATE TABLE IF  NOT EXISTS jeu(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nom VARCHAR NOT NULL,
     PRIMARY KEY(id)
@@ -55,3 +58,28 @@ CREATE TABLE jeu(
 )
 
 CHARACTER SET 'utf8';
+ENGIN=INNOB;
+
+
+
+
+----------------------------------Clé Etrangère----------------------------------/
+
+--- Score --- 
+ALTER TABLE score
+ADD CONSTRAINT fk_score_utilisateur FOREIGN KEY (id_joueur) REFERENCES utilisateur(id) ON DELETE CASCADE;
+
+ALTER TABLE score
+ADD CONSTRAINT fk_score_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE CASCADE;
+
+
+--- Message ---
+ALTER TABLE message
+ADD CONSTRAINT fk_message_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE CASCADE;
+
+ALTER TABLE message
+ADD CONSTRAINT fk_message_utilisateur FOREIGN KEY (id_expediteur) REFERENCES utilisateur(id) ON DELETE CASCADE;
+
+
+
+
