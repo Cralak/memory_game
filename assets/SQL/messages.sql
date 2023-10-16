545f28d75c548f23badc1613d512d081dfb442a0
Proutalax <proutalax@codings-MacBook-Pro.local>
2023-10-16 17:24:35 +0200

    ALL FIXED

--- assets/SQL/messages.sql
+++ assets/SQL/messages.sql
@@ -1,32 +1,32 @@
-INSERT INTO messages(sender_id, game_id, message, message_date_time)
+INSERT INTO messages(sender_id, game_id, message, message_date_and_time)
 VALUES('1', '1', 'Bonjour', '2002-10-10 10:19:56'),
     ('1', '1', 'Salut', '2002-10-10 10:20:56'),
     ('1', '1', 'Jjajjaj', '2002-10-10 10:19:56'),
     ('1', '1', 'magab', '2019-10-10 10:19:56'),
     ('1', '1', 'jouef', '2005-10-10 10:19:56');
 
-INSERT INTO messages(sender_id, game_id, message, message_date_time)
+INSERT INTO messages(sender_id, game_id, message, message_date_and_time)
 VALUES('2', '1', 'jajajajaj', '2002-10-10 10:19:56'),
     ('2', '1', 'Mamoumimama', '2002-10-10 10:20:56'),
     ('2', '1', 'gounbaagaga', '2002-10-10 10:19:56'),
     ('2', '1', 'kekekekekekek', '2019-10-10 10:19:56'),
     ('2', '1', 'MOLABABABABAATORORO', '2005-10-10 10:19:56');
 
-INSERT INTO messages(sender_id, game_id, message, message_date_time)
+INSERT INTO messages(sender_id, game_id, message, message_date_and_time)
 VALUES('3', '1', 'RIMOU RIMAMA TOUTOU BATAPA', '2002-10-10 10:19:56'),
     ('3', '1', 'JUIFOUFOU', '2002-10-10 10:20:56'),
     ('3', '1', 'MOUSLINE', '2002-10-10 10:19:56'),
     ('3', '1', 'RIGABO', '2019-10-10 10:19:56'),
     ('3', '1', '15', '2005-10-10 10:19:56');
 
-INSERT INTO messages(sender_id, game_id, message, message_date_time)
+INSERT INTO messages(sender_id, game_id, message, message_date_and_time)
 VALUES('4', '1', 'JSP RAHHHHH', '2002-10-10 10:19:56'),
     ('4', '1', 'NONONONONO', '2002-10-10 10:20:56'),
     ('4', '1', 'niq', '2002-10-10 10:19:56'),
     ('4', '1', 'jabed', '2019-10-10 10:19:56'),
     ('4', '1', 'raminou', '2005-10-10 10:19:56');
 
-INSERT INTO messages(sender_id, game_id, message, message_date_time)
+INSERT INTO messages(sender_id, game_id, message, message_date_and_time)
 VALUES('5', '1', 'ZIZI!!!', '2002-10-10 10:19:56'),
     ('5', '1', 'JOUEUF', '2002-10-10 10:20:56'),
     ('5', '1', 'KFJBSF', '2002-10-10 10:19:56'),
