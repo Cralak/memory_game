60d21ddedd6d3714c2b55454cf2f85c39b04a0c4
ouachtou <pierre.legrand1105@gmail.com>
2023-10-16 15:58:04 +0200

    sql_update

--- assets/SQL/allo.sql
+++ assets/SQL/allo.sql
@@ -1,7 +1,7 @@
------------------- TABLE UTILISATEUR ----------------/
+---------------------------------- TABLE UTILISATEUR SCORE MESSAGE JEU---------------------------------/
 
 
-CREATE TABLE IF  NOT EXISTS utilisateur(
+CREATE TABLE IF NOT EXISTS utilisateur(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     email VARCHAR(191) UNIQUE NOT NULL,
     mot_de_passe VARCHAR(200) NOT NULL,
@@ -12,30 +12,27 @@ CREATE TABLE IF  NOT EXISTS utilisateur(
 )
 
 CHARACTER SET 'utf8';
-ENGIN=INNOB;
+ENGINE=INNODB;
 
 
 
-
----------------- TABLE SCORE ----------------/
-
-CREATE TABLE IF  NOT EXISTS score(
+CREATE TABLE IF NOT EXISTS score(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     id_joueur INT UNSIGNED NOT NULL,
     id_jeu INT UNSIGNED NOT NULL,
-    difficulte ENUM('FACILE,MOYEN,DIFFICIL') NOT NULL,
+    difficulte ENUM('FACILE','MOYEN','DIFFICIL') NOT NULL,
     score_partie INT UNSIGNED NOT NULL,
     datetime_partie DATETIME NOT NULL,
     PRIMARY KEY(id),
 )
 
 CHARACTER SET 'utf8';
-ENGIN=INNOB;
+ENGINE=INNODB;
 
 
---------------- TABLE MESSAGE ----------------/
 
-CREATE TABLE IF  NOT EXISTS message(
+
+CREATE TABLE IF NOT EXISTS messages(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     id_expediteur INT UNSIGNED NOT NULL,
     id_jeu INT UNSIGNED NOT NULL,
@@ -45,12 +42,11 @@ CREATE TABLE IF  NOT EXISTS message(
 )
 
 CHARACTER SET 'utf8';
-ENGIN=INNOB;
+ENGINE=INNODB;
 
 
----------------- TABLE JEU ----------------/
 
-CREATE TABLE IF  NOT EXISTS jeu(
+CREATE TABLE IF NOT EXISTS jeu(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nom VARCHAR NOT NULL,
     PRIMARY KEY(id)
@@ -58,7 +54,7 @@ CREATE TABLE IF  NOT EXISTS jeu(
 )
 
 CHARACTER SET 'utf8';
-ENGIN=INNOB;
+ENGINE=INNODB;
 
 
 
@@ -74,12 +70,30 @@ ADD CONSTRAINT fk_score_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE CA
 
 
 --- Message ---
-ALTER TABLE message
+ALTER TABLE messages
 ADD CONSTRAINT fk_message_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE CASCADE;
 
-ALTER TABLE message
+ALTER TABLE messages
 ADD CONSTRAINT fk_message_utilisateur FOREIGN KEY (id_expediteur) REFERENCES utilisateur(id) ON DELETE CASCADE;
 
 
+---------------------------------- Insertions ----------------------------------/
 
+INSERT INTO utilisateur(email, mot_de_passe, pseudo, date_et_heure, derniere_connexion)
+VALUES('cralakgaming@gmail.com', 'iLoveGaming', 'Cralak', '2019-10-10', NULL),
+('loma@gmail.com', 'loma55', 'lomaDeezNutz', '2018-01-10', NULL),
+('proutox@gmail.com', 'pwoutow', 'ProutoxLeGamer', '2021-10-05', NULL),
+('viande@gmail.com', 'FreinCassé69', 'LeGrandViande', '2001-11-09', NULL),
+('borkevgolem@gmail.com', 'FreinTropCourt', 'brokevo', '2016-02-11', NULL);
+
+INSERT INTO utilisateur(email,mot_de_passe,pseudo,date_et_heure)
+VALUES('drocsidgaming@gmail.com', '1234', 'Drocsid', '2010-10-10');
+
+UPDATE utilisateur
+SET email = 'drocsidddddddgaming@gmail.com' AND SET pseudo = "XtremDrocsid"
+WHERE id = 4;
+
+UPDATE utilisateur
+SET pseudo = "XtremDrocsid" 
+WHERE id = 4;
 
