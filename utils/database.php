6a937d2297a5fcd7d96e08282f6e5cff737f6f84
Proutalax <proutalax@codings-MacBook-Pro.local>
2023-10-26 17:33:18 +0200

    Revert "Merge branch 'Drocsid' into cralak"
    This reverts commit 5969e56c5292a6628646b6f782acfd72c0a7230f, reversing
    changes made to 5b9a112694dba59bd0b7bb83c484555974cf1e2c.

--- utils/database.php
+++ utils/database.php
@@ -1,11 +1,11 @@
 <?php
-function connectToDbAndPOSTPdo(): PDO
+function connectToDbAndGetPdo(): PDO
 {
-$dbname = 'projet_flash';
+$dbname = 'memory';
 $host = 'localhost';
 $dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
 $user = 'root';
-$pass = '';
+$pass = 'root';
 $driver_options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
@@ -18,100 +18,29 @@ $driver_options = [
     }
 
 }
+$pdo = connectToDbAndGetPdo();
 
-$pdo = connectToDbAndPOSTPdo();
 
 
-/*--------------INSERT-------------*/
 function insertusers($pdo, $pseudo, $email, $motDePasse ) :void {
 
     $insertUser = $pdo-> prepare('INSERT INTO users(username, email, pass) VALUES (:username, :email, :pass)');
     $insertUser -> execute([':email' => $email, ':pass' => $motDePasse, ':username' => $pseudo]);}
 
-/*---------UNIQUE---------*/
+
 function uniquePseudo($pdo,$pseudo):bool{
-
-    $pseudoUsed = $pdo-> prepare('SELECT username FROM users WHERE username = :username');
-    $pseudoUsed->execute([':username' => $pseudo]);
-    $pseudoUtilise = $pseudoUsed-> fetch();
-
-    return $pseudoUtilise != NULL;
+    $pseudoUsed = $pdo-> prepare('SELECT username FROM users WHERE username =:nom');
+    $pseudoUsed->execute([':nom' => $pseudo]);
+    $pseudoUsed-> fetch();
+    return $pseudoUsed != NULL;
 }
 
 function uniqueEmail($pdo,$email):bool{
-
     $emailUsed = $pdo-> prepare('SELECT email FROM users WHERE email = :email');
     $emailUsed->execute([':email' => $email]);
-    $emailUtilise = $emailUsed-> fetch();
-
-    return $emailUtilise != NULL;
-
-}
-
-/*----------UPADATE----------*/
-function updateEmail($pdo,$newEmail,$userId) :void {
-    $newerEmail = $pdo -> prepare('UPDATE users SET email = :email WHERE id = :id');
-    $newerEmail->execute([':email' => $newEmail,':id' => $userId]); 
-}
-
-function updatePassword($pdo,$newPassword,$userId) :void {
-    $newerPassword = $pdo -> prepare('UPDATE users SET pass = :pass WHERE id = :id');
-    $newerPassword -> execute([':pass' => $newPassword,':id' => $userId]); 
+    $emailUtilise= $emailUsed-> fetch();
+    return $emailUsed != NULL;
 }
 
 
-/*------------SELECT----------*/
-
-
-function PasswordUse($pdo,$password) : bool {
-    $PasswordUsed = $pdo->prepare('SELECT pass FROM users WHERE pass = :pass');
-    $PasswordUsed->execute([':pass' => $password]);
-    $Password_utilise = $PasswordUsed -> fetch();
-    return isset($Password_utilise -> pass);
-}
-
-
-function emailUse($pdo,$email) : bool {
-    $emailUsed = $pdo->prepare('SELECT email FROM users WHERE email = :email');
-    $emailUsed->execute([':email' => $email]);
-    $email_utilise = $emailUsed -> fetch();
-    return isset($email_utilise -> mail);
-}
-
-
-
-function oldEmail($pdo, $Email,$userId): bool
-{
-    $OldEmail = $pdo->prepare('SELECT email FROM users WHERE id = :id;');
-    $OldEmail -> execute(['id' => $userId]);
-    $actualEmail = $OldEmail -> fetchColumn();
-
-    return $actualEmail == $Email;
-    
-}
-
-function oldPassword($pdo,$Password, $userId): bool {   
-
-    $oldPassword = $pdo->prepare('SELECT pass FROM users WHERE id = :id');
-    $oldPassword->execute(['id' => $userId]);
-    $currentPassword = $oldPassword -> fetchColumn();
-    $Password = hash('sha256', $Password);
-
-    
-    return $currentPassword == $Password;
-}
-
-
-/*---------------CONNEXION------------*/
-
-function login($pdo, $email, $password): void {
-$pdoStatement = $pdo->prepare('SELECT id FROM users WHERE email = :email AND pass = :pass');
-$pdoStatement->execute([":email" => $email, ":pass" => $password]);
-$userInfo = $pdoStatement->fetch();
-
-if($userInfo){
-    $_SESSION['userId'] = $userInfo->id;
-    header("location: game/memory/memory.php");       
-    }
-}
 ?>
